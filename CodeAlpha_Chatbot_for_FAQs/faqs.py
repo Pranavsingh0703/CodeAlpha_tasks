c8f2c2bfcf4bebd3894913347925bfa20d17c492
Pranav singh <189369200+Pranavsingh0703@users.noreply.github.com>
2025-12-26 11:41:18 +0530

    Create faqs.py

---
+++ CodeAlpha_Chatbot_for_FAQs/faqs.py
@@ -0,0 +1,16 @@
+faqs = {
+    "What is CodeAlpha?":
+        "CodeAlpha is a Software platform that provides internships and project-based learning opportunities.",
+
+    "How can I apply for an internship?":
+        "You can apply for an internship through the CodeAlpha official website.",
+
+    "Is the internship paid?":
+        "Most internships at CodeAlpha are unpaid and focus on learning.",
+
+    "What is the duration of internship?":
+        "The internship duration is usually one month.",
+
+    "Do I get a certificate?":
+        "Yes, you receive a certificate after successful completion of the internship."
+}
